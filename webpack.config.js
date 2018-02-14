1ae45123d14c9a3a83a20ef4383d8a0b2f3ff02f
skill <frontendmonster@gmail.com>
2018-02-15 03:05:58 +0330

    Add client and webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          babelrc: true
+        }
+      }
+    ]
+  }
+};
